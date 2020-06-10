5401954b8e9f98f76d648255868e27c124dd9db2
elenadimitrova <elena@arenabg.com>
2020-06-10 17:38:12 +0300

    Remove receive from BaseWallet

--- contracts/wallet/BaseWallet.sol
+++ contracts/wallet/BaseWallet.sol
@@ -155,10 +155,4 @@ contract BaseWallet is IWallet {
             }
         }
     }
-
-    receive() external payable {
-        if (msg.value > 0) {
-            emit Received(msg.value, msg.sender, msg.data);
-        }
-    }
 }
